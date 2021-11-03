ab259902f444d36437c7ede9d58473820f71ef64
Felix Gruber <felgru@posteo.net>
2021-11-03 19:09:35 +0100

    Add type annotations to Git interface

--- git.py
+++ git.py
@@ -1,10 +1,12 @@
-# SPDX-FileCopyrightText: 2020 Felix Gruber <felgru@posteo.net>
+# SPDX-FileCopyrightText: 2020–2021 Felix Gruber <felgru@posteo.net>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from abc import ABCMeta, abstractmethod
 import os
+from pathlib import Path
 import subprocess
+from typing import Iterable, Optional, Sequence, Union
 
 
 class BaseGit(metaclass=ABCMeta):
@@ -19,19 +21,20 @@ class BaseGit(metaclass=ABCMeta):
     def current_branch(self) -> str: pass
 
     @abstractmethod
-    def change_branch(self, branch) -> None: pass
+    def change_branch(self, branch: str) -> None: pass
 
     @abstractmethod
-    def add_file(self, file) -> None: pass
+    def add_file(self, file: Union[Path, str]) -> None: pass
 
     @abstractmethod
-    def add_files(self, files) -> None: pass
+    def add_files(self, files: Iterable[Union[Path, str]]) -> None: pass
 
     @abstractmethod
-    def add_files_to_annex(self, files) -> None: pass
+    def add_files_to_annex(self, files: Sequence[Union[Path, str]]) -> None:
+        pass
 
     @abstractmethod
-    def commit(self, message) -> None: pass
+    def commit(self, message: str) -> None: pass
 
     @abstractmethod
     def reset_index_and_working_directory(self) -> None: pass
@@ -39,27 +42,32 @@ class BaseGit(metaclass=ABCMeta):
 
 class Git(BaseGit):
 
-    def __init__(self, work_tree, git_dir):
+    def __init__(self,
+                 work_tree: Union[Path, str],
+                 git_dir: Union[Path, str]):
         self.git_command = ['git', f'--work-tree={work_tree}',
                                    f'--git-dir={git_dir}']
         # was git annex initialized in the Git repository?
-        self._has_annex = os.path.exists(os.path.join(git_dir, 'annex'))
+        self._has_annex = Path(git_dir, 'annex').exists()
 
-    def has_annex(self):
+    def has_annex(self) -> bool:
         return self._has_annex
 
-    def _run_git_command(self, args, input=None):
+    def _run_git_command(self,
+                         args: list[str],
+                         input: Optional[str] = None) -> str:
         return subprocess.run(self.git_command + args,
                               capture_output=True, encoding='UTF8',
                               input=input, check=True).stdout
 
-    def working_directory_is_clean(self):
+    def working_directory_is_clean(self) -> bool:
         return not self._has_files_with_any_of_these_status_flags('MADRCU')
 
-    def has_staged_files(self):
+    def has_staged_files(self) -> bool:
         return self._has_files_with_any_of_these_status_flags('ADRC')
 
-    def _has_files_with_any_of_these_status_flags(self, flags):
+    def _has_files_with_any_of_these_status_flags(self,
+                                                  flags: Iterable[str]) -> bool:
         status = self._run_git_command(['status', '--porcelain'])
         change_flags = set(flags)
         for line in status.split('\n')[:-1]:
@@ -68,58 +76,59 @@ class Git(BaseGit):
                 return True
         return False
 
-    def current_branch(self):
+    def current_branch(self) -> str:
         return self._run_git_command(['branch', '--show-current']).strip()
 
-    def change_branch(self, branch):
+    def change_branch(self, branch: str) -> None:
         self._run_git_command(['checkout', branch])
 
-    def add_file(self, file):
-        self._run_git_command(['add', file])
+    def add_file(self, file: Union[Path, str]) -> None:
+        self._run_git_command(['add', str(file)])
 
-    def add_files(self, files):
-        self._run_git_command(['add'] + files)
+    def add_files(self, files: Iterable[Union[Path, str]]) -> None:
+        self._run_git_command(['add', *(str(file) for file in files)])
 
-    def add_files_to_annex(self, files):
-        if not (self.has_annex() and files):
+    def add_files_to_annex(self, files: Sequence[Union[Path, str]]) -> None:
+        if not (self.has_annex() and len(files) > 0):
             return
-        self._run_git_command(['annex', 'add', '--force-large'] + files)
+        self._run_git_command(['annex', 'add', '--force-large',
+                               *(str(file) for file in files)])
 
-    def commit(self, message):
+    def commit(self, message: str) -> None:
         self._run_git_command(['commit', '--file=-'], input=message)
 
-    def reset_index_and_working_directory(self):
+    def reset_index_and_working_directory(self) -> None:
         self._run_git_command(['reset', '--hard', 'HEAD'])
 
 
 class FakeGit(BaseGit):
 
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
-    def working_directory_is_clean(self):
+    def working_directory_is_clean(self) -> bool:
         return True
 
-    def has_staged_files(self):
+    def has_staged_files(self) -> bool:
         return False
 
-    def current_branch(self):
+    def current_branch(self) -> str:
         return 'not using Git'
 
-    def change_branch(self, branch):
+    def change_branch(self, branch: str) -> None:
         pass
 
-    def add_file(self, file):
+    def add_file(self, file: Union[Path, str]) -> None:
         pass
 
-    def add_files(self, files):
+    def add_files(self, files: Iterable[Union[Path, str]]) -> None:
         pass
 
-    def add_files_to_annex(self, files):
+    def add_files_to_annex(self, files: Sequence[Union[Path, str]]) -> None:
         pass
 
-    def commit(self, message):
+    def commit(self, message: str) -> None:
         pass
 
-    def reset_index_and_working_directory(self):
+    def reset_index_and_working_directory(self) -> None:
         pass
--- import_transaction.py
+++ import_transaction.py
@@ -1,12 +1,32 @@
-# SPDX-FileCopyrightText: 2020 Felix Gruber <felgru@posteo.net>
+# SPDX-FileCopyrightText: 2020–2021 Felix Gruber <felgru@posteo.net>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from contextlib import contextmanager
 import os
-from typing import Iterable, Optional, TypeVar, Union
+from pathlib import Path
+from typing import Iterable, Iterator, Optional, Protocol, TypeVar, Union
 
 from git import BaseGit
+
+
+class ImportTransactionProtocol(Protocol):
+
+    def begin(self, import_branch: str) -> None: ...
+
+    def commit(self, commit_message: Optional[str] = None) -> None: ...
+
+    def rollback(self) -> None: ...
+
+    def add_file(self, file: Union[Path, str]) -> None: ...
+
+    def add_files(self, files: Iterable[Union[Path, str]]) -> None: ...
+
+    def move_file_to_annex(self,
+                           source: Union[Path, str],
+                           dest: Union[Path, str]) -> None: ...
+
+    def set_commit_message(self, commit_message: str) -> None: ...
 
 
 class ImportTransaction:
@@ -18,8 +38,8 @@ class ImportTransaction:
         assert self.git.working_directory_is_clean()
         self.old_branch = self.git.current_branch()
         self.git.change_branch(import_branch)
-        self.files_to_move_to_annex: list[tuple[str, str]] = []
-        self.files_to_add_to_git: list[str] = []
+        self.files_to_move_to_annex: list[tuple[Path, Path]] = []
+        self.files_to_add_to_git: list[Path] = []
 
     def commit(self, commit_message: Optional[str] = None) -> None:
         if commit_message is None:
@@ -43,14 +63,16 @@ class ImportTransaction:
         self.git.change_branch(self.old_branch)
         del self.old_branch
 
-    def add_file(self, file: str) -> None:
-        self.files_to_add_to_git.append(file)
+    def add_file(self, file: Union[Path, str]) -> None:
+        self.files_to_add_to_git.append(Path(file))
 
-    def add_files(self, files: Iterable[str]) -> None:
-        self.files_to_add_to_git.extend(files)
+    def add_files(self, files: Iterable[Union[Path, str]]) -> None:
+        self.files_to_add_to_git.extend(Path(file) for file in files)
 
-    def move_file_to_annex(self, source: str, dest: str) -> None:
-        self.files_to_move_to_annex.append((source, dest))
+    def move_file_to_annex(self,
+                           source: Union[Path, str],
+                           dest: Union[Path, str]) -> None:
+        self.files_to_move_to_annex.append((Path(source), Path(dest)))
 
     def set_commit_message(self, commit_message: str) -> None:
         self.commit_message = commit_message
@@ -64,8 +86,8 @@ class FakeImportTransaction:
     def begin(self, import_branch: str) -> None:
         print("beginning fake import Git transaction on branch "
               + import_branch)
-        self.files_to_move_to_annex: list[tuple[str, str]] = []
-        self.files_to_add_to_git: list[str] = []
+        self.files_to_move_to_annex: list[tuple[Path, Path]] = []
+        self.files_to_add_to_git: list[Path] = []
 
     def commit(self, commit_message: Optional[str] = None) -> None:
         if commit_message is None:
@@ -76,14 +98,17 @@ class FakeImportTransaction:
     def rollback(self) -> None:
         print("rolling back fake Git import transaction")
 
-    def add_file(self, file: str) -> None:
+    def add_file(self, file: Union[Path, str]) -> None:
         print(f"adding file {file} to import transaction")
 
-    def add_files(self, files: Iterable[str]) -> None:
-        print(f"adding files {', '.join(files)} to import transaction")
+    def add_files(self, files: Iterable[Union[Path, str]]) -> None:
+        print(f"adding files {', '.join(str(f) for f in files)} "
+              "to import transaction")
 
-    def move_file_to_annex(self, source: str, dest: str) -> None:
-        self.files_to_move_to_annex.append((source, dest))
+    def move_file_to_annex(self,
+                           source: Union[Path, str],
+                           dest: Union[Path, str]) -> None:
+        self.files_to_move_to_annex.append((Path(source), Path(dest)))
         print(f"moving {source} to {dest}")
 
     def set_commit_message(self, commit_message: str) -> None:
@@ -91,8 +116,11 @@ class FakeImportTransaction:
 
 
 @contextmanager
-def import_transaction(git: BaseGit, import_branch: str, dry_run: bool):
-    transaction: Union[FakeImportTransaction, ImportTransaction]
+def import_transaction(
+        git: BaseGit,
+        import_branch: str,
+        dry_run: bool) -> Iterator[ImportTransactionProtocol]:
+    transaction: ImportTransactionProtocol
     if dry_run:
         transaction = FakeImportTransaction(git)
     else:
@@ -108,7 +136,3 @@ def import_transaction(git: BaseGit, import_branch: str, dry_run: bool):
             transaction.commit()
         else:
             transaction.rollback()
-
-ImportTransactionProtocol = TypeVar('ImportTransactionProtocol',
-                                    ImportTransaction,
-                                    FakeImportTransaction)
