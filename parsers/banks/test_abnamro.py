da64efdfd8b1c3df63cd90fd566590195a9c5354
Felix Gruber <felgru@posteo.net>
2022-06-05 19:21:10 +0200

    abnamro: Add tests for BEA description parser.
    This includes a currently failing test for BEA transactions with
    currency exchange provided by Dmitry Mukhin in #1.

---
+++ parsers/banks/test_abnamro.py
@@ -0,0 +1,59 @@
+# SPDX-FileCopyrightText: 2022 Felix Gruber <felgru@posteo.net>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from datetime import date
+from decimal import Decimal
+
+from .abnamro import DescriptionParser
+
+
+def test_parsing_bea_transaction() -> None:
+    description = ["BEA, Betaalpas",
+                   "My example store,PAS123",
+                   "NR:123ABC   01.01.22/12.23",
+                   "LOCATION"]
+
+    parser = DescriptionParser(currency='EUR',
+                               account='assets:bank:checking:ABN AMRO')
+    transaction = parser.parse(
+            description=description,
+            bookdate=date(2022, 1, 1),
+            value_date=date(2022, 1, 1),
+            amount=Decimal("1.23"),
+            )
+    assert transaction.description == 'My example store'
+    m = transaction.metadata
+    assert m['store'] == 'My example store'
+    assert m['pas_nr'] == '123'
+    assert m['NR'] == '123ABC'
+    assert m['date'] == '01.01.22'
+    assert m['time'] == '12:23'
+    assert m['location'] == 'LOCATION'
+
+
+def test_parsing_bea_transaction_with_currency_exchange() -> None:
+    description = ["BEA, Google Pay",
+                   "My example store,PAS123",
+                   "NR:123ABC   01.01.22/12.23",
+                   "DRNIS,Land: HR",
+                   "HRK 44,00 1EUR=7,4074074 HRK",
+                   "ECB Koers=7,5620027 OPSLAG 2,09%",
+                   "KOSTEN •0,15 ACHTERAF BEREKEND"]
+
+    parser = DescriptionParser(currency='EUR',
+                               account='assets:bank:checking:ABN AMRO')
+    transaction = parser.parse(
+            description=description,
+            bookdate=date(2022, 1, 1),
+            value_date=date(2022, 1, 1),
+            amount=Decimal("1.23"),
+            )
+    assert transaction.description == 'My example store'
+    m = transaction.metadata
+    assert m['store'] == 'My example store'
+    assert m['pas_nr'] == '123'
+    assert m['NR'] == '123ABC'
+    assert m['date'] == '01.01.22'
+    assert m['time'] == '12:23'
+    assert m['location'] == 'DRNIS,Land: HR'
