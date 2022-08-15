731b4bc1a15a88e815e8597d17e4a7c447305563
Felix Gruber <felgru@posteo.net>
2022-08-15 19:02:52 +0200

    import-bank-statements.py: Add --reimport option.
    The new --reimport option takes a bank name (or the word 'all') as an
    argument to re-run the bank statement parser on all files belonging to
    that bank (or to all banks, when 'all' is given instead of a bank name).
    
    Like a normal run of import-bank-statements.py, this option works on the
    import branch and merges to the previously checked-out branch unless
    --no-merge is specified.

--- import-bank-statements.py
+++ import-bank-statements.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import sys
 from typing import Any, Optional
 
-from config import ImportConfig
+from config import ImportConfig, LedgerConfig
 from git import BaseGit, FakeGit, Git, GitMergeConflictError
 from import_transaction import (
         DirtyWorkingDirectoryException,
@@ -77,14 +77,22 @@ def sort_incoming_statements_to_ledger_dirs(
     return classified
 
 
+class ParserConfigs(defaultdict[type[Parser], BaseParserConfig]):
+    def __init__(self, ledger_dir: Path):
+        self.rules_dir = ledger_dir / 'rules'
+    def __missing__(self, key: type[Parser]) -> BaseParserConfig:
+        config = key.config_type.load(self.rules_dir)
+        self[key] = config
+        return config
+
+
 def import_incoming_statements(incoming_statements: list[IncomingStatement],
                                ledger_dir: Path,
                                git: BaseGit,
                                import_branch: str,
                                force: bool,
                                dry_run: bool) -> None:
-    rules_dir = ledger_dir / 'rules'
-    configs: dict[type[Parser], BaseParserConfig] = {}
+    configs = ParserConfigs(ledger_dir)
     with import_transaction(git, import_branch, dry_run) as transaction:
         import_summary = dict()
         by_bank: dict[str, list[IncomingStatement]] = defaultdict(list)
@@ -107,9 +115,7 @@ def import_incoming_statements(incoming_statements: list[IncomingStatement],
                 if not dry_run:
                     dest_dir.mkdir(parents=True, exist_ok=True)
                 dest_file = (dest_dir / f).with_suffix('.hledger')
-                config = configs.get(type(incoming.parser))
-                if config is None:
-                    config = incoming.parser.config_type.load(rules_dir)
+                config = configs[type(incoming.parser)]
                 if parse_and_write_bank_statement(incoming.parser,
                                                   src_file, dest_file,
                                                   config,
@@ -185,15 +191,19 @@ def merge_dateranges(dateranges: list[tuple[date, date]]) -> None:
             dateranges.pop(i+1)
 
 
-def regenerate_includes(work_dir: Path, config: ImportConfig) -> None:
+def get_ledger_config_containing_dir(work_dir: Path,
+                                     config: ImportConfig) -> LedgerConfig:
     for ledger_config in config.ledgers.values():
         if work_dir.is_relative_to(ledger_config.ledger_dir):
-            break
+            return ledger_config
     else:
         print(f'Current working directory {work_dir} is not inside any known'
-              ' ledger path.')
+              ' ledger path.', file=sys.stderr)
         exit(1)
 
+
+def regenerate_includes(work_dir: Path, config: ImportConfig) -> None:
+    ledger_config = get_ledger_config_containing_dir(work_dir, config)
     print(f'Regenerate includes in {ledger_config.ledger_dir}.')
     # change working directory for git status to work correctly
     os.chdir(ledger_config.ledger_dir)
@@ -208,76 +218,152 @@ def regenerate_includes(work_dir: Path, config: ImportConfig) -> None:
     write_include_files(ledger_config.ledger_dir, git)
 
 
-def main() -> None:
-    aparser = argparse.ArgumentParser(
-            description='import account statement PDFs into hledger')
-    aparser.add_argument('--force', dest='force', default=False,
-                         action='store_true',
-                         help='overwrite existing ledgers')
-    aparser.add_argument('--dry-run', dest='dry_run',
-                         default=False, action='store_true',
-                         help='run parsers without writing any output files')
-    aparser.add_argument('--regenerate-includes', dest='regenerate_includes',
-                         default=False, action='store_true',
-                         help='only regenerate include files; don\'t import '
-                              'new bank statements')
-    aparser.add_argument('--no-merge', dest='merge',
-                         default=True, action='store_false',
-                         help='don\'t merge import branch after import')
+class Main:
+    def __init__(self):
+        aparser = argparse.ArgumentParser(
+                description='import account statement PDFs into hledger')
+        aparser.add_argument('--force', dest='force', default=False,
+                             action='store_true',
+                             help='overwrite existing ledgers')
+        aparser.add_argument('--dry-run', dest='dry_run',
+                             default=False, action='store_true',
+                             help='run parsers without writing any output files')
+        aparser.add_argument('--regenerate-includes', dest='regenerate_includes',
+                             default=False, action='store_true',
+                             help='only regenerate include files; don\'t import '
+                                  'new bank statements')
+        aparser.add_argument('--reimport',
+                             default=None,
+                             help='reimport bank statements from given bank '
+                                  + ' ({})'.format(', '.join(sorted(parsers)))
+                                  + '; or "all" to reimport from all banks.')
+        aparser.add_argument('--no-merge', dest='merge',
+                             default=True, action='store_false',
+                             help='don\'t merge import branch after import')
 
-    args = aparser.parse_args()
+        self.args = aparser.parse_args()
 
-    xdg = getXDGdirectories('bank-statement-parser')
-    config_file = xdg['config'] / 'import.cfg'
-    config = ImportConfig.read_from_file(config_file)
+        self.xdg = getXDGdirectories('bank-statement-parser')
+        self.config_file = self.xdg['config'] / 'import.cfg'
+        self.config = ImportConfig.read_from_file(self.config_file)
 
-    if args.regenerate_includes:
-        regenerate_includes(Path.cwd(), config)
-        exit(0)
+        if self.args.reimport is not None and self.args.regenerate_includes:
+            print('--regenerate-includes cannot be specified together with '
+                  '--reimport.', file=sys.stderr)
+            exit(1)
 
-    selection_script = xdg['config'] / 'select_ledger.py'
-    select_ledger: Callable[[BankStatementMetadata], str]
-    if selection_script.exists():
-        with open(selection_script, 'r') as f:
-            content = f.read()
-            parse_globals: dict[str, Any] = {
-                'BankStatementMetadata': BankStatementMetadata,
-                }
-            exec(compile(content, selection_script, 'exec'), parse_globals)
-            if 'select_ledger' not in parse_globals:
-                print(f'{selection_script} doesn\'t contain select_ledger'
-                      ' function.',
-                      file=sys.stderr)
-                exit(1)
-            select_ledger = parse_globals['select_ledger']
-    elif len(config.ledgers) == 1:
-        ledger_name = next(iter(config.ledgers))
-        def select_ledger(meta: BankStatementMetadata) -> str:
-            return ledger_name
-    else:
-        print(f'Error: {config_file} contains more than one ledger,'
-              f' but {selection_script} is missing.',
-              file=sys.stderr)
-        exit(1)
-    incoming_statements = get_metadata_of_incoming_statements(
-            config.incoming_dir)
-    classified = sort_incoming_statements_to_ledger_dirs(
-            incoming_statements,
-            select_ledger,
-            )
-    if any(key not in config.ledgers for key in classified.keys()):
-        for key, statements in classified.items():
-            if key in config.ledgers:
-                continue
-            mismatched_files = ', '.join(str(s.statement_path)
-                                         for s in statements)
-            print(f'Error: {mismatched_files} were assigned to unknown ledger'
-                  f' configuration {key}. Please check {selection_script}.',
+        mode: Callable[[], None]
+        if self.args.regenerate_includes:
+            mode = self.regenerate_includes
+        elif self.args.reimport is not None:
+            mode = self.reimport_bank_statements
+        else:
+            mode = self.import_new_bank_statements
+        self.selected_mode = mode
+
+    def run(self) -> None:
+        self.selected_mode()
+
+    def regenerate_includes(self) -> None:
+        regenerate_includes(Path.cwd(), self.config)
+
+    def import_new_bank_statements(self) -> None:
+        selection_script = self.xdg['config'] / 'select_ledger.py'
+        select_ledger: Callable[[BankStatementMetadata], str]
+        if selection_script.exists():
+            with open(selection_script, 'r') as f:
+                content = f.read()
+                parse_globals: dict[str, Any] = {
+                    'BankStatementMetadata': BankStatementMetadata,
+                    }
+                exec(compile(content, selection_script, 'exec'), parse_globals)
+                if 'select_ledger' not in parse_globals:
+                    print(f'{selection_script} doesn\'t contain select_ledger'
+                          ' function.',
+                          file=sys.stderr)
+                    exit(1)
+                select_ledger = parse_globals['select_ledger']
+        elif len(self.config.ledgers) == 1:
+            ledger_name = next(iter(self.config.ledgers))
+            def select_ledger(meta: BankStatementMetadata) -> str:
+                return ledger_name
+        else:
+            print(f'Error: {self.config_file} contains more than one ledger,'
+                  f' but {selection_script} is missing.',
                   file=sys.stderr)
-        exit(1)
-    for key, statements in classified.items():
-        ledger_config = config.ledgers[key]
-        print(f'Importing bank statements to {ledger_config.ledger_dir}.')
+            exit(1)
+        incoming_statements = get_metadata_of_incoming_statements(
+                self.config.incoming_dir)
+        classified = sort_incoming_statements_to_ledger_dirs(
+                incoming_statements,
+                select_ledger,
+                )
+        if any(key not in self.config.ledgers for key in classified.keys()):
+            for key, statements in classified.items():
+                if key in self.config.ledgers:
+                    continue
+                mismatched_files = ', '.join(str(s.statement_path)
+                                             for s in statements)
+                print(f'Error: {mismatched_files} were assigned to unknown ledger'
+                      f' configuration {key}. Please check {selection_script}.',
+                      file=sys.stderr)
+            exit(1)
+        for key, statements in classified.items():
+            ledger_config = self.config.ledgers[key]
+            print(f'Importing bank statements to {ledger_config.ledger_dir}.')
+            # change working directory for git status to work correctly
+            os.chdir(ledger_config.ledger_dir)
+            git: BaseGit
+            if ledger_config.git_dir is not None:
+                git = Git(ledger_config.ledger_dir, ledger_config.git_dir)
+                import_branch = ledger_config.import_branch
+            else:
+                git = FakeGit()
+                import_branch = git.current_branch()
+
+            try:
+                import_incoming_statements(statements,
+                                           ledger_config.ledger_dir,
+                                           git, import_branch,
+                                           self.args.force, self.args.dry_run)
+            except DirtyWorkingDirectoryException:
+                print(f'{ledger_config.ledger_dir} contains uncommitted changes,'
+                      ' please commit those before continuing.', file=sys.stderr)
+                exit(1)
+            # The import_transaction in import_incoming_statements automatically
+            # resets the branch to the previously checked-out one after importing
+            # to the import_branch.
+            if (self.args.merge
+                    and isinstance(git, Git)
+                    and import_branch != git.current_branch()):
+                try:
+                    git.merge(import_branch)
+                except GitMergeConflictError as e:
+                    conflicting_files = [ledger_config.ledger_dir / c.name
+                                         for c in e.conflicts]
+                    not_autogenerated = [p for p in conflicting_files
+                                         if p.name != 'journal.hledger']
+                    if not_autogenerated:
+                        raise RuntimeError(
+                                'Could not automerge the following files:\n'
+                                + '\n'.join(str(p) for p in not_autogenerated))
+                    write_include_files(ledger_config.ledger_dir, git)
+                    git.commit(f"Merge branch '{import_branch}'")
+
+    def reimport_bank_statements(self) -> None:
+        bank_name = self.args.reimport
+        if bank_name == 'all':
+            selected_parsers = dict(parsers.items())
+        else:
+            selected_parser = parsers.get(bank_name)
+            if selected_parser is None:
+                print(f'Unknown bank name: {bank_name}.', file=sys.stderr)
+                exit(1)
+            selected_parsers = {bank_name: selected_parser}
+            del selected_parser
+        ledger_config = get_ledger_config_containing_dir(Path.cwd(),
+                                                         self.config)
+        print(f'Reimport bank statements in {ledger_config.ledger_dir}.')
         # change working directory for git status to work correctly
         os.chdir(ledger_config.ledger_dir)
         git: BaseGit
@@ -288,19 +374,33 @@ def main() -> None:
             git = FakeGit()
             import_branch = git.current_branch()
 
-        try:
-            import_incoming_statements(statements,
-                                       ledger_config.ledger_dir,
-                                       git, import_branch,
-                                       args.force, args.dry_run)
-        except DirtyWorkingDirectoryException:
-            print(f'{ledger_config.ledger_dir} contains uncommitted changes,'
-                  ' please commit those before continuing.', file=sys.stderr)
-            exit(1)
-        # The import_transaction in import_incoming_statements automatically
-        # resets the branch to the previously checked-out one after importing
-        # to the import_branch.
-        if (args.merge
+        parser_configs = ParserConfigs(ledger_config.ledger_dir)
+
+        with import_transaction(git, import_branch,
+                                self.args.dry_run) as transaction:
+            for year in sorted(dir for dir in ledger_config.ledger_dir.iterdir()
+                               if dir.is_dir() and dir.name.isnumeric()):
+                for subdir in sorted(dir for dir in year.iterdir()
+                                     if dir.is_dir()):
+                    if subdir.name.isnumeric(): # month
+                        updated_ledgers: list[Path] = []
+                        for bankdir in sorted(dir for dir in subdir.iterdir()
+                                              if dir.is_dir()):
+                            updated_ledgers.extend(
+                                self.reimport_bank_dir(bankdir,
+                                                       selected_parsers,
+                                                       parser_configs))
+                    else:
+                        updated_ledgers = \
+                            self.reimport_bank_dir(subdir, selected_parsers,
+                                                   parser_configs)
+                    git.add_files(updated_ledgers)
+            commit_message = f'reimport {bank_name} bank statements'
+            transaction.set_commit_message(commit_message)
+
+        # The import_transaction automatically resets the branch to the
+        # previously checked-out one after importing to the import_branch.
+        if (self.args.merge
                 and isinstance(git, Git)
                 and import_branch != git.current_branch()):
             try:
@@ -318,5 +418,32 @@ def main() -> None:
                 git.commit(f"Merge branch '{import_branch}'")
 
 
+    def reimport_bank_dir(self,
+                          bank_dir: Path,
+                          parsers: dict[str, dict[str, type[Parser]]],
+                          parser_configs: dict[type[Parser], BaseParserConfig],
+                          ) -> list[Path]:
+        bank_parsers = parsers.get(bank_dir.name)
+        if bank_parsers is None:
+            return []
+        files_with_parser = sorted(
+            (f, parser)
+            for f in bank_dir.iterdir()
+            if f.is_file()
+               and (parser := bank_parsers.get(f.suffix.lower())) is not None
+        )
+        reimported_ledgers: list[Path] = []
+        for file_, parser in files_with_parser:
+            bank_statement = parser(file_).parse(parser_configs[parser])
+            ledger_file = file_.with_suffix('.hledger')
+            if not ledger_file.exists():
+                continue
+            print(f'Reimporting {file_}')
+            with ledger_file.open('w') as f:
+                bank_statement.write_ledger(f)
+            reimported_ledgers.append(ledger_file)
+        return reimported_ledgers
+
+
 if __name__ == '__main__':
-    main()
+    Main().run()
